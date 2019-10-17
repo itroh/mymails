68e8bcf1557cb827f6cdf41a02a4beddda0ed560
Hortense <hortense@pelay.fr>
2019-10-17 14:01:33 +0200

    ajout message OK

--- script.js
+++ script.js
@@ -14,9 +14,64 @@ for(var i =0;i<document.getElementsByClassName('trash').length;i++){
 
     });
 }
+
+
+
 //gestion bouton add
 document.getElementById("btn-add").addEventListener("click",function(){
     console.log(document.getElementById("add-message").value);
+    nouveauMail(document.getElementById("add-message").value);
 })
 
 //creation fonction qui créer une row message avec avatar and co
+function nouveauMail(contenuMessage){
+    var newDiv=document.createElement("div");
+    //ajout attr class="row" à cette nouvelle div
+    var attrDivClass=document.createAttribute("class");
+    attrDivClass.value="row";
+    newDiv.setAttributeNode(attrDivClass);
+   
+    // Avatar
+    var newImg=document.createElement("img");
+    var attrImgClass=document.createAttribute("class");
+    attrImgClass.value="avatar";
+    newImg.setAttributeNode(attrImgClass);
+    var attrImgSrc=document.createAttribute("src");
+    attrImgSrc.value="avatar-1.jpg";
+    newImg.setAttributeNode(attrImgSrc);
+    //placement img ds div
+    newDiv.appendChild(newImg);
+        
+    // gestion nom
+   var newDiv2=document.createElement("div");
+   var nomAvatar=document.createElement("h6")
+   nomAvatar.textContent="Hortense Guéneau";
+   newDiv2.appendChild(nomAvatar);
+
+   //gestion texte du message
+   var textMessage=document.createElement("p")
+   var attrMsgClass=document.createAttribute("class");
+   attrMsgClass.value="descr";
+   textMessage.setAttributeNode(attrMsgClass);
+   textMessage.textContent=contenuMessage;
+   newDiv2.appendChild(textMessage);
+   
+   //ajout de la div
+   newDiv.appendChild(newDiv2);
+
+    //place la div au bon endroit
+    var placement=document.getElementsByClassName("row")[0];
+    document.body.insertBefore(newDiv,placement);
+    //ajout trash
+    var newTrash=document.createElement("img");
+    var attrTrashClass=document.createAttribute("class");
+    attrTrashClass.value="trash";
+    newTrash.setAttributeNode(attrTrashClass);
+    var attrTrashSrc=document.createAttribute("src");
+    attrTrashSrc.value="trash.png";
+    newTrash.setAttributeNode(attrTrashSrc);
+    newDiv.appendChild(newTrash);
+    
+
+
+}
